1c435109c11b066584b22129b5a516fa62983e09
NicoHadisurya <114816055+NicoHadisurya@users.noreply.github.com>
2023-04-27 23:29:45 +0700

    Final Edit

--- Final_Project/src/ProjectFile/GUI.java
+++ Final_Project/src/ProjectFile/GUI.java
@@ -1,10 +1,32 @@
 package ProjectFile;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
 public class GUI {
 
 	public Main() {
 		// TODO Auto-generated constructor stub
-		
+		Scanner sc = new Scanner(system.in)
+
+		f.setTitle("Database Menu");
+		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
+
+		JButton b1 = new JButton();
+		b1.setText("Insert new Menu");
+		JButton b2 = new JButton("View Menu");
+		JButton b3 = new JButton("Update Menu");
+		JButton b4 = new JButton("Delete Menu");
 	}
 
 	public static void main(String[] args) {
